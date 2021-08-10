44a5bc14bac3e85109890521f4182489499d0489
yusif763 <yusifhuseynli1105@gmail.com>
2021-08-10 15:13:10 +0400

    commit

--- static/js/main.js
+++ static/js/main.js
@@ -1,7 +1,5 @@
 url = "/api/v1/core/all-courses/";
 
-localStorage.removeItem("course_title")
-
 getCourseList = () => {
     fetch(url)
     .then((resp) => resp.json())
@@ -20,15 +18,15 @@ getCourseList = () => {
           document.querySelector(".flexxx").innerHTML += `
           
           <div class="col-md-3 mb-3 col-12">
-          <div class="card" style=" height: 370px;">
+          <div class="card" style=" height: 350px;">
           <a href="/course/${element.id}" style="color: black; text-decoration: none;">
   <div class="card-img-top" style="background: url(${element.image}); height: 170px; background-size: cover;"></div>
 </a>
 
   <div class="card-body">
-  <h4 class="mt-3" style="font-size: 20px;">${element.title}</h4>
-  <p style="height: 50px; overflow: scroll;">${element.description}</p>
-  <h6 class="card-text">${element.price} AZN</h6>
+  <h4 class="mt-3">${element.title}</h4>
+  <p>${element.description}</p>
+  <h6 class="card-text">${element.price}AZN</h6>
   </div>
 </div>
           </div>
@@ -65,7 +63,7 @@ getInternshipList = () => {
         
         <div class="col-md-3 col-12" style="padding: 50px 30px;">
         <div style="background: url(${element.image}); background-size: cover; background-repeat: no-repeat; height: 350px; padding: 20px; padding-top: 250px; border-radius: 3px;">
-          <h3 style="color: white; font-weight: 200;">Price ${element.price} AZN</h3>
+          <h3 style="color: white; font-weight: 200;">Price ${element.price}AZN</h3>
           <span style="color: white;">${element.title}</span>
           </div>
         </div>
@@ -76,43 +74,43 @@ getInternshipList = () => {
   })
 }
 
-getBTaskList = () => {
-  fetch(url)
-  .then((resp) => resp.json())
-  .then((data) => {
-    let courses = []
-    data.forEach(e => {
-      console.log(e.category);
-      if (e.category == 3 && e.is_shared == true) {
-        courses.push(e)
-      }
-    })
-    console.log(courses);
-    console.log(courses.slice(0,4));
-    console.log(document.querySelector("#fleximg3"));
-    courses.slice(0,4).forEach(element => {
-        document.querySelector("#fleximg3").innerHTML += `
+// getBTaskList = () => {
+//   fetch(url)
+//   .then((resp) => resp.json())
+//   .then((data) => {
+//     let courses = []
+//     data.forEach(e => {
+//       console.log(e.category);
+//       if (e.category == 3 && e.is_shared == true) {
+//         courses.push(e)
+//       }
+//     })
+//     console.log(courses);
+//     console.log(courses.slice(0,4));
+//     console.log(document.querySelector("#fleximg3"));
+//     courses.slice(0,4).forEach(element => {
+//         document.querySelector("#fleximg3").innerHTML += `
 
-        <div class="col-md-3 mb-3 col-12">
-          <div class="card" style=" height: 370px;">
-          <a href="/course/${element.id}" style="color: black; text-decoration: none;">
-  <div class="card-img-top" style="background: url(${element.image}); height: 170px; background-size: cover;"></div>
-</a>
+//         <div class="col-md-3 mb-3 col-12">
+//           <div class="card" style=" height: 350px;">
+//           <a href="/course/${element.id}" style="color: black; text-decoration: none;">
+//   <div class="card-img-top" style="background: url(${element.image}); height: 170px; background-size: cover;"></div>
+// </a>
 
-  <div class="card-body">
-  <h4 class="mt-3" style="font-size: 20px;">${element.title}</h4>
-  <p style="height: 50px; overflow: scroll;">${element.description}</p>
-  <h6 class="card-text">${element.price} AZN</h6>
-  </div>
-</div>
-          </div>
+//   <div class="card-body">
+//   <h4 class="mt-3">${element.title}</h4>
+//   <p style="height: 50px; overflow: scroll;">${element.description}</p>
+//   <h6 class="card-text">AZN${element.price}</h6>
+//   </div>
+// </div>
+//           </div>
         
         
       
-        `
-    });
-  })
-}
+//         `
+//     });
+//   })
+// }
 
 // url_users = "http://localhost:8000/api/v1/show-users/";
 
